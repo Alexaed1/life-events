b5da97965543a3a1a104da2cead045f770b8b108
Alexaed1 <107372316+Alexaed1@users.noreply.github.com>
2022-06-28 08:56:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const myInformation = {
+name: "Alexa",
+age: 25,
+birthday: "August 9",
+pineapplePizza: "true",
+lifeEvents: ["I like to sing", "I have a dog", "I went to Wayne State", "I moved to Northville"]
+}
+
+if (myInformation.pineapplePizza ) {
+    console.log(`My name is ${name} and I like pineapples on pizza. I am currently ${age}years old and my birthday is on ${birthday}.`)
+} else {
+    console.log (`My name is ${name} and I like pineapples on pizza. I am currently ${age}years old and my birthday is on ${birthday}.`)
+};
+
+for(let i=0; i <=4; lifeEvents; i++) {
+    console.log(lifeEvents);
+}
+
+let counter = 0;
+while (counter < 3) { console.log( counter ); i++;
+}
+
+Math.floor(Math.random() * 10) + 1
